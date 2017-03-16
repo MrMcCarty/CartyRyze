32858e7be7d8eeaf6ef0b22bc45acc739e984f47
MrMcCarty <seppl125@gmail.com>
2017-03-16 11:22:09 +0100

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,12 +1,10 @@
  <?php
-     $json = json_decode(file_get_contents('php://input')); //allow you to get all content
-     
-     
-     $value_1 = $json->data[0]->Object1; // get the object in question (table start to 0)
-     $value_2 = $json->data[1]->Object2;
- 
-     
-     echo json_encode(array('YourValue1'=>$value_1, 'YourValue2'=>$value_2)); //don't forget to re-encode the array
+foreach($_REQUEST as $fieldName=>$fieldVal)
+{
+   $container = explode("=", $fieldName);
+   if($container[0] =="t")
+   {
+    echo trim($container[1]);
+   }
+}
  ?>
- 
- 
